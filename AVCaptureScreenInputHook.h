99c922ad4a53b12f4befaacdd0b576b9c92da85a
tipray <fangyuan@ldsafe.com>
2020-03-11 17:16:15 +0800

    asdasdasdasdad

---
+++ AVCaptureScreenInputHook.h
@@ -0,0 +1,13 @@
+//
+//  AVCaptureScreenInputHook.h
+//  trld_trlib
+//
+//  Created by Chen on 11/04/2017.
+//  Copyright © 2017 liuhui. All rights reserved.
+//
+
+#ifndef AVCaptureScreenInputHook_h
+#define AVCaptureScreenInputHook_h
+void hookAVCaptureScreenInput(void);
+
+#endif /* AVCaptureScreenInputHook_h */
